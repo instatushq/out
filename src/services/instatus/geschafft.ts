79b3813f2b6302299ac6cf12c292cce9fde896eb
Slayer <51095261+GhostSlayer@users.noreply.github.com>
2021-02-21 14:31:53 +0200

    Create geschafft.ts

---
+++ src/services/instatus/geschafft.ts
@@ -0,0 +1,9 @@
+import InstatusService from '../base/instatus'
+
+class Geschafft extends InstatusService {
+  constructor() {
+    super('Geschafft', 'https://geschafft.instatus.com')
+  }
+}
+
+export default Geschafft
