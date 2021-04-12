1135c8f78ba503089540f7a5dd6abd908f28e42f
Francois Best <francois@francoisbest.com>
2021-04-12 18:08:54 +0200

    feat: Add Buttondown

---
+++ src/services/statuspage/buttondown.ts
@@ -0,0 +1,9 @@
+import StatusPageService from '../base/statuspage'
+
+class Buttondown extends StatusPageService {
+  constructor() {
+    super('Buttondown', 'https://status.buttondown.email', 'ncz6z93t15wp')
+  }
+}
+
+export default Buttondown
